dfed777787acc4461445c8304e52452c5c896218
Luis de Dios Martín <luisddm@gmail.com>
2017-04-09 20:58:21 +0200

    Improve interface

--- src/js/app.js
+++ src/js/app.js
@@ -13,11 +13,14 @@ osc.frequency.value = 0;
 
 function handleOrientation({ absolute, alpha, beta, gamma }) {
 
-  const value = Math.round(gamma);
+  const yAxis = Math.round(gamma);
+  $.id('y-axis').innerHTML = yAxis;
 
-  $.id('hola').innerHTML = value;
-  osc.frequency.value = Math.abs(value * 10);
-  $.id('body').style.backgroundColor = value > 10 ? 'lightpink' : 'lightblue';
+  const freq = Math.abs(yAxis * 10);
+  $.id('freq').innerHTML = freq;
+
+  osc.frequency.value = freq;
+  $.id('body').style.backgroundColor = Math.abs(yAxis) > 10 ? 'lightpink' : 'lightblue';
 }
 
 function start(event) {
