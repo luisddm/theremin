b5d54f3c672e7c164a31c7b5b19e19b8d48a48a5
Luis de Dios Martín <luisddm@gmail.com>
2017-04-09 12:52:54 +0200

    adjust package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -21,26 +21,26 @@ gulp.task('es6', () =>
         filename: 'bundle.min.js',
       },
     }, webpack))
-    .pipe(babel({ presets: ['es2015'] }))   // Transpile ES6+ into ES5
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))             // Copy to dist
+    .pipe(gulp.dest('dist/js'))
 );
 
 gulp.task('html', () =>
-  gulp.src('src/index.html')     // Take index.html
+  gulp.src('src/index.html')
     .pipe(htmlReplace({
-      css: 'css/bundle.min.css',   // Replace the stylesheet links section with the link to the new styleshet
-      js: 'js/bundle.min.js',      // Replace the script links section with the link to the new script
+      css: 'css/bundle.min.css',
+      js: 'js/bundle.min.js',
     }))
-    .pipe(gulp.dest('dist'))    // Copy to dist
+    .pipe(gulp.dest('dist'))
 );
 
 gulp.task('css', () =>
-  gulp.src('src/css/styles.css')     // Take CSS files
-    .pipe(rename('bundle.min.css'))  // Concat
+  gulp.src('src/css/app.css')
+    .pipe(rename('bundle.min.css'))
     .pipe(postCSS([postCSSImport]))
-    .pipe(cleanCSS())                // Minify
-    .pipe(gulp.dest('dist/css'))     // Copy to dist
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'))
 );
 
 gulp.task('eslint', () =>
@@ -52,8 +52,8 @@ gulp.task('clean', () =>
 );
 
 gulp.task('browser-sync', () => {
-  browserSync.init(['dist/css/**.css', 'dist/js/**.js', 'dist/**.html'], {  // Look for changes in dist directories
-    server: 'dist',  // Reload browser when any JS is modified or inject CSS when any stylesheet is modified
+  browserSync.init(['dist/css/**.css', 'dist/js/**.js', 'dist/**.html'], {
+    server: 'dist',
   });
 });
 
@@ -63,4 +63,4 @@ gulp.task('default', ['html', 'css', 'es6', 'browser-sync'], () => {
   gulp.watch('src/*.html', ['html']);
 });
 
-gulp.task('build', ['html', 'css', 'es6', 'size']);
+gulp.task('build', ['html', 'css', 'es6']);
